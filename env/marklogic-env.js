258ebf66a0ce1aaf27306b9e634121229cb20fd4
Praveen <tvnpraveen@gmail.com>
2017-04-13 00:17:32 -0400

    test bug2

--- env/marklogic-env.js
+++ env/marklogic-env.js
@@ -1,7 +1,7 @@
 var env =  {
   database: "Documents", // Each connection can specify its own database
   host: "localhost",     // The host against which queries will be run
-  port: 8000,            // By default port 8000 accepts Client API requests
+  port: 32773,            // By default port 8000 accepts Client API requests
   user: "admin",        // Our newly-created user with at least the rest-writer role
   password: "admin",  // writer's password
   authType: "DIGEST"     // The default auth
